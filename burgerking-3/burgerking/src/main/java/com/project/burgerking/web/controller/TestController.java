ec66ca8113f517f783fae417b3aaf927263e2233
깃허브 테스트 <>
2023-07-11 20:36:25 +0900

    project git test

--- burgerking-3/burgerking/src/main/java/com/project/burgerking/web/controller/TestController.java
+++ burgerking-3/burgerking/src/main/java/com/project/burgerking/web/controller/TestController.java
@@ -1,4 +1,5 @@
 package com.project.burgerking.web.controller;
+//gitTest
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
